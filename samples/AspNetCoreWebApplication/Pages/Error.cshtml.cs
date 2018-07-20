be84c68fe13a458c54a8cc33900502c9f76923aa
Bill Boga <billboga@outlook.com>
2018-07-20 13:08:02 -0400

    Update to ASP.NET Core 2.1.1
    Fixes issues with previous commits that updated libary, but sample project was on an older version of the Core runtime. There are breaking changes between the minor versions.
    
    Implements https://github.com/billbogaiv/hybrid-model-binding/issues/11

--- samples/AspNetCoreWebApplication/Pages/Error.cshtml.cs
+++ samples/AspNetCoreWebApplication/Pages/Error.cshtml.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
 namespace AspNetCoreWebApplication.Pages
 {
--- samples/AspNetCoreWebApplication/Startup.cs
+++ samples/AspNetCoreWebApplication/Startup.cs
@@ -2,7 +2,7 @@ using HybridModelBinding;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
