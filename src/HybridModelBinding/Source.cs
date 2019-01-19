bca16de8d41d18e281407fac4e03d6cebbf4b3e0
Bill Boga <billboga@outlook.com>
2019-01-19 18:45:45 -0500

    Add Header ValueProvider
    New behavior mimics existing ASP.NET MVC behavior of QueryString and Form value providers.
    Not really sure why this isn't included in that package.
    Didn't do thorough testing, but did notice one gotcha if identical header-keys are submitted: the values are combined as a single comma-separated string.
    
    Implements https://github.com/billbogaiv/hybrid-model-binding/issues/23

--- src/HybridModelBinding/Source.cs
+++ src/HybridModelBinding/Source.cs
@@ -4,6 +4,7 @@
     {
         public const string Body = nameof(Body);
         public const string Form = nameof(Form);
+        public const string Header = nameof(Header);
         public const string QueryString = nameof(QueryString);
         public const string Route = nameof(Route);
     }
