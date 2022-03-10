7aa924ec28d94e0dec6133a90fe2ffb83424a191
Misiu <t.jagusz@gmail.com>
2022-03-10 11:21:44 +0100

    support json property name attributes

---
+++ src/HybridModelBinding/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HybridModelBinding.Extensions
+{
+    public static class PropertyInfoExtensions
+    {
+        public static string GetNameFromAttributes(this PropertyInfo propertyInfo)
+        {
+            //default value
+            var name = propertyInfo.Name;
+
+            //newtonsoft.json
+            var jsonPropertyAttribute = propertyInfo.GetCustomAttribute<JsonPropertyAttribute>();
+            if (jsonPropertyAttribute != null)
+            {
+                return jsonPropertyAttribute.PropertyName;
+            }
+            //system.text.json
+            var jsonPropertyNameAttribute = propertyInfo.GetCustomAttribute<JsonPropertyNameAttribute>();
+            if (jsonPropertyNameAttribute != null)
+            {
+                return jsonPropertyNameAttribute.Name;
+            }
+
+            return name;
+        }
+    }
+}
